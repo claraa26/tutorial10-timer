71e611ba8f0076e15a80e2cab30ff0ba4ca842d8
claraa26 <clarasistaa26@gmail.com>
2024-05-08 09:55:15 +0700

    Experiment 1.3: Multiple Spawn and removing drop

--- README.md
+++ README.md
@@ -9,4 +9,11 @@
 Diketahui bahwa pemanggilan howdy dan done terjadi secara asynchronous, dan akan dieksekusi setelah diproses oleh executor
 sedangkan hey hey terdapat pada program utama dan sebelum executor, sehingga memberikan output seperti di gambar.
 
+### 1.3. Multiple Spawn and removing drop
+![](https://imgur.com/0plf8Oo.jpg)
 
+Pada kali ini saya telah memodifikasi untuk membuat spawner.spawn menjadi ada 3 dan menghapus drop(spawner). Pada output
+tercetak howdy terlebih dahulu sebanyak 3 kali baru tercetak done. Hal tersebut karna program berjalan secara asychronous. 
+Executor akan mengekseskusi taks pertama terlebih dahulu hingga selesai yaitu output howdy baru task selanjutnya hingga 
+done tereksekusi. Sedangkan penghapusan drop(spawner) mengakibatkan program tidak berhenti, karena program menunggu 
+task-task selanjutnya.
--- src/main.rs
+++ src/main.rs
@@ -24,11 +24,23 @@ fn main() {
         TimerFuture::new(Duration::new(2, 0)).await;
         println!("Clara's computer: done!");
     });
+    spawner.spawn(async {
+        println!("Clara's computer: howdy!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Clara's computer: done!");
+    });
+    spawner.spawn(async {
+        println!("Clara's computer: howdy!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Clara's computer: done!");
+    });
     println!("Clara's computer: hey hey");
 
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
-    drop(spawner);
+    // drop(spawner);
 
     // Run the executor until the task queue is empty.
     // This will print "howdy!", pause, and then print "done!".
